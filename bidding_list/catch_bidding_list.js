6b9a0de8914a1f45383944c196d1d041a729aa26
shenks0628 <91179867+shenks0628@users.noreply.github.com>
2023-11-15 20:02:01 +0800

    Update catch_bidding_list.js

--- bidding_list/catch_bidding_list.js
+++ bidding_list/catch_bidding_list.js
@@ -25,38 +25,6 @@ const db = getFirestore(app);
 
 let bidsData;
 
-function exit_add(docId) {
-    let display_list = document.getElementById("display_list");
-    display_list.innerHTML = "";
-    for (let key of Object.keys(bidsData)) {
-        console.log(bidsData[key]);
-        const productId = key; // 替換成實際的產品 ID
-        console.log(productId);
-        // 使用 doc 函數構建該產品的參考路徑
-        const productRef = doc(db, "products", productId);
-
-        // 使用 getDoc 函數取得該產品的文件快照
-        getDoc(productRef)
-        .then((productDoc) => {
-            if (productDoc.exists()) {
-                // 取得該產品的資料
-                const productData = productDoc.data();
-                display_list.innerHTML += '<div class="product" id="' + productId + '"><img src="' + productData.imgs[0] + '" alt="product"><h3>' + productData.name + 
-                                            '</h3><p>實時競價：<a class="price">' + productData.price + '</a></p><p id="padd' + productId + '"><button class="btn" type="submit" id="add' + productId + '">加注</button></p><p><button class="btn" type="submit" id="exit' + productId + '">退出</button></p>';
-                console.log("Product data for product with ID", productId, ":", productData);
-            }
-            else {
-                console.log("Product with ID", productId, "does not exist.");
-            }
-        })
-        .catch((error) => {
-            console.error("Error getting product document:", error);
-        });
-    }
-    display_list.removeEventListener("click", handleCheck);
-    display_list.addEventListener("click", handleCheck);
-}
-
 function confirm_add(docId) {
     const productRef = doc(db, "products", docId);
     getDoc(productRef)
@@ -96,7 +64,7 @@ function add(docId) {
             const productData = productDoc.data();
             console.log("Product data for product with ID", docId, ":", productData);
             let p = document.getElementById("padd" + docId);
-            p.innerHTML = '<input type="text" id="price' + docId + '" class="input_text" name="search" value="' + (parseInt(productData.price) + 50) + '"></input><button class="btn" type="submit" id="exit_add' + docId + '">取消加注</button><button class="btn" type="submit" id="confirm_add' + docId + '">確認加注</button>';
+            p.innerHTML = '<input type="text" id="price' + docId + '" class="input_text" name="search" value="' + (parseInt(productData.price) + 50) + '"></input><button class="btn" type="submit" id="confirm_add' + docId + '">確認加注</button>';
         }
         else {
             console.log("Product with ID", docId, "does not exist.");
@@ -140,10 +108,6 @@ const handleCheck = (event) => {
     else if (targetId.startsWith("confirm_add")) {
         const productId = targetId.slice(11);
         confirm_add(productId);
-    }
-    else if (targetId.startsWith("exit_add")) {
-        const productId = targetId.slice(8);
-        exit_add(productId);
     }
 }
 
