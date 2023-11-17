5ff0447166c1310b6e99a68ff7d5e063a41ba261
shenks0628 <cooljoseph100@gmail.com>
2023-11-17 19:50:55 +0800

    function def

--- cart/cart.js
+++ cart/cart.js
@@ -4,7 +4,7 @@ import { collection, doc, setDoc, getDoc, getDocs, query, orderBy, limit, where,
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
