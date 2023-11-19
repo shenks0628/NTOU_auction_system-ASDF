f1bd9b262579d2f6235f48ebfdc93f1300a3a2c7
shenks0628 <cooljoseph100@gmail.com>
2023-11-19 11:35:18 +0800

    make it live XD

--- firebase.js
+++ firebase.js
@@ -24,6 +24,27 @@ const analytics = getAnalytics(app);
 const auth = getAuth();
 const db = getFirestore(app);
 
+const display = async () => {
+    const latest_normal = document.getElementById("latest_normal");
+    const latest_bids = document.getElementById("latest_bids");
+    const q_noraml = query(collection(db, "products"), where("type", "==", "normal"), orderBy("time", "desc"), limit(20));
+    const q_bids = query(collection(db, "products"), where("type", "==", "bids"), orderBy("time", "desc"), limit(20));
+    const queryNormalSnapshot = await getDocs(q_noraml);
+    const queryBidsSnapshot = await getDocs(q_bids);
+    latest_normal.innerHTML = "";
+    latest_bids.innerHTML = "";
+    queryNormalSnapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        const productData = doc.data();
+        latest_normal.innerHTML += '<div class="product" id="' + doc.id + '"><a href"https://youtu.be/JRZQGMzbPes?si=A6IdLTDFrLgRxKXn"><img src="' + productData.imgs[0] + '" alt="product"></a><h3>' + productData.name +  '</h3><p>不二價：</p><p class="price">' + productData.price + '</p><button class="btn">加入購物車</button></div>';
+    });
+    queryBidsSnapshot.forEach((doc) => {
+        console.log(doc.id, "=>", doc.data());
+        const productData = doc.data();
+        latest_bids.innerHTML += '<div class="product" id="' + doc.id + '"><a href="https://youtu.be/JRZQGMzbPes?si=A6IdLTDFrLgRxKXn"><img src="' + productData.imgs[0] + '" alt="product"></a><h3>' + productData.name +  '</h3><p>不二價：</p><p class="price">' + productData.price + '</p><button class="btn">加入競標清單</button></div>';
+    });
+}
+
 const start = () => {
     const login = document.getElementById("login");
     onAuthStateChanged(auth, (user) => {
@@ -37,3 +58,4 @@ const start = () => {
 }
 
 window.addEventListener("load", start);
+window.addEventListener("load", display);
