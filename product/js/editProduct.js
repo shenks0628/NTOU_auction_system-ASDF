c42ce919c2a1fdd91c1ab04b5735c2f890b75f8e
DHugo324 <hugodai0324@gmail.com>
2023-11-20 00:56:18 +0800

    add editProduct page
    Function not implemented

---
+++ product/js/editProduct.js
@@ -0,0 +1,126 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, updateProfile, signOut, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
+import { getFirestore, collection, doc, setDoc, getDoc, addDoc, getDocs, query, orderBy, limit, where, onSnapshot, deleteDoc, updateDoc, arrayUnion, arrayRemove, serverTimestamp, deleteField } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyClpUY1NfcCO_HEHPOi6ma9RXdsSxCGWy4",
+    authDomain: "ntou-auction-system-112eb.firebaseapp.com",
+    projectId: "ntou-auction-system-112eb",
+    storageBucket: "ntou-auction-system-112eb.appspot.com",
+    messagingSenderId: "320414610227",
+    appId: "1:320414610227:web:0ec7e2571126d3b2fd4446",
+    measurementId: "G-FLXQ2BQCZF"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+const db = getFirestore(app);
+
+var srcs;
+var imgs;
+function start() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let key = "7Pl4V9WEFdM09Sa7NMEy";
+    if (urlParams.get('id')) {
+        key = urlParams.get('id');
+    }
+    const productId = key; // 替換成實際的產品 ID
+    // 使用 doc 函數構建該產品的參考路徑
+    const productRef = doc(db, "products", productId);
+    // 使用 getDoc 函數取得該產品的文件快照
+    getDoc(productRef)
+        .then((productDoc) => {
+            if (productDoc.exists()) {
+                // 取得該產品的資料
+                const productData = productDoc.data();
+                console.log("Product data for product with ID", productId, ":", productData);
+                var str = productData.name.trim().split("#");
+                var itemName = document.getElementById("itemName");
+                itemName.innerHTML = str[0];
+                var itemInfo = document.getElementById("itemInfo");
+                itemInfo.innerHTML = productData.description;
+                var itemTag = document.getElementById("itemTag");
+                console.log(str);
+                if (str.length == 1) itemTag.innerHTML = "無";
+                else {
+                    itemTag.innerHTML = "";
+                    for (var i = 1; i < str.length; i++) {
+                        itemTag.innerHTML += str[i];
+                        if (i != str.length - 1) itemTag.innerHTML += ", "
+                    }
+                }
+
+                srcs = productData.imgs;
+                imgs.setAttribute("src", srcs[0]);
+            }
+            else {
+                console.log("Product with ID", productId, "does not exist.");
+            }
+        })
+        .catch((error) => {
+            console.error("Error getting product document:", error);
+        });
+
+    imgs = document.getElementById("itemPicture");
+    imgs.addEventListener("click", changeImage, false);
+
+    document.getElementById("searchButton").addEventListener("click", searchProduct, false);
+    document.getElementById("ToDiscription").addEventListener("click", changeInfo, false);
+    document.getElementById("ToComment").addEventListener("click", changeInfo, false);
+
+    const login = document.getElementById("login");
+    const profileImage = document.getElementById("profileImage");
+    onAuthStateChanged(auth, (user) => {
+        console.log(user);
+        if (user) {
+            login.innerHTML = "登出";
+            profileImage.setAttribute("src", user.photoURL);
+        }
+        else {
+            login.innerHTML = "登入";
+        }
+    });
+
+};
+
+function searchProduct() {
+    var form = document.getElementById("searchForm");
+    var url = "../search/search.html?keyword=" + form.elements["keyword"].value;
+    console.log(url);
+    window.alert("暫未開放此功能");
+    // window.location.href = url;
+}
+
+function changeImage() {
+    for (let i = 0; i < srcs.length; i++) {
+        if (srcs[i] == imgs.getAttribute("src")) {
+            imgs.setAttribute("src", srcs[(i + 1) % srcs.length]);
+            break;
+        }
+    }
+}
+
+function changeInfo() {
+    if (document.getElementById("description").style.display == "none") {
+        document.getElementById("ToDiscription").disabled = true;
+        document.getElementById("ToComment").disabled = false;
+        document.getElementById("description").style.display = "block";
+        document.getElementById("review").style.display = "none";
+    }
+    else {
+        document.getElementById("ToDiscription").disabled = false;
+        document.getElementById("ToComment").disabled = true;
+        document.getElementById("description").style.display = "none";
+        document.getElementById("review").style.display = "block";
+    }
+}
+
+window.addEventListener("load", start, false);
+
+// 初始化時顯示購物車內容
