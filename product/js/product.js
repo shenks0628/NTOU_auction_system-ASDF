5e1330c52d8384da14d550703e752ac43845df7b
DHugo324 <hugodai0324@gmail.com>
2023-11-20 02:14:55 +0800

    add eventListener

--- product/js/product.js
+++ product/js/product.js
@@ -24,9 +24,12 @@ const db = getFirestore(app);
 
 var srcs;
 var imgs;
+var userID = "none", productOwnerID = "none";
+var login, profileImage;
 function start() {
+    eventSetting();
     const urlParams = new URLSearchParams(window.location.search);
-    let key = "7Pl4V9WEFdM09Sa7NMEy";
+    let key = "dd6VioVhhtD3p6P2r49r";
     if (urlParams.get('id')) {
         key = urlParams.get('id');
     }
@@ -39,13 +42,17 @@ function start() {
             if (productDoc.exists()) {
                 // 取得該產品的資料
                 const productData = productDoc.data();
+                productOwnerID = productData.seller;
+                setting(userID, productOwnerID);
                 console.log("Product data for product with ID", productId, ":", productData);
-                var str = productData.name.trim().split("#");
-                var itemName = document.getElementById("itemName");
+                let str = productData.name.trim().split("#");
+                let itemName = document.getElementById("itemName");
                 itemName.innerHTML = str[0];
-                var itemInfo = document.getElementById("itemInfo");
-                itemInfo.innerHTML = productData.description;
-                var itemTag = document.getElementById("itemTag");
+                let itemDescription = document.getElementById("itemDescription");
+                itemDescription.innerHTML = productData.description;
+                let itemPrice = document.getElementById("itemPrice");
+                itemPrice.innerHTML = "$" + productData.price.toString();
+                let itemTag = document.getElementById("itemTag");
                 if (str.length == 1) itemTag.innerHTML = "無";
                 else {
                     itemTag.innerHTML = "";
@@ -66,38 +73,73 @@ function start() {
             console.error("Error getting product document:", error);
         });
 
-    imgs = document.getElementById("itemPicture");
-    imgs.addEventListener("click", changeImage, false);
 
-    document.getElementById("searchButton").addEventListener("click", searchProduct, false);
-    document.getElementById("ToDiscription").addEventListener("click", changeInfo, false);
-    document.getElementById("ToComment").addEventListener("click", changeInfo, false);
-
-    const login = document.getElementById("login");
-    const profileImage = document.getElementById("profileImage");
+    login = document.getElementById("login");
+    profileImage = document.getElementById("profileImage");
     onAuthStateChanged(auth, (user) => {
         console.log(user);
         if (user) {
+            userID = user.email;
             login.innerHTML = "登出";
             profileImage.setAttribute("src", user.photoURL);
         }
         else {
+            userID = "none";
             login.innerHTML = "登入";
         }
+        setting(userID, productOwnerID);
     });
-
 };
+function eventSetting() {
+    imgs = document.getElementById("itemPicture"),
+        imgs.addEventListener("click", changeImage, false);
+    document.getElementById("searchButton").addEventListener("click", searchProduct, false);
+    document.getElementById("ToDiscription").addEventListener("click", changeInfo, false);
+    document.getElementById("ToComment").addEventListener("click", changeInfo, false);
+
+    document.getElementById("editName").addEventListener("click", editName, false);
+    document.getElementById("editDescription").addEventListener("click", editDescription, false);
+    document.getElementById("editPrice").addEventListener("click", editPrice, false);
+    // document.getElementById("edit").addEventListener("click", edit, false);
+    document.getElementById("editTag").addEventListener("click", editTag, false);
+}
+
+function setting() {
+    // console.log(userID, productOwnerID);
+    if (productOwnerID == "none") return;
+    if (userID == "none") {
+        document.getElementById("cart").style.display = "none";
+        let edit = document.getElementsByClassName("edit");
+        for (var i = 0; i < edit.length; i++) {
+            edit[i].style.display = "none";
+        }
+    }
+    else if (userID == productOwnerID && productOwnerID != "none") {
+        document.getElementById("cart").style.display = "none";
+        let edit = document.getElementsByClassName("edit");
+        for (var i = 0; i < edit.length; i++) {
+            edit[i].style.display = "inline";
+        }
+    }
+    else {
+        document.getElementById("cart").style.display = "block";
+        let edit = document.getElementsByClassName("edit");
+        for (var i = 0; i < edit.length; i++) {
+            edit[i].style.display = "none";
+        }
+    }
+}
 
 function searchProduct() {
-    var form = document.getElementById("searchForm");
-    var url = "../search/search.html?keyword=" + form.elements["keyword"].value;
+    let form = document.getElementById("searchForm");
+    let url = "../search/search.html?keyword=" + form.elements["keyword"].value;
     console.log(url);
     window.alert("暫未開放此功能");
     // window.location.href = url;
 }
 
 function changeImage() {
-    for (let i = 0; i < srcs.length; i++) {
+    for (var i = 0; i < srcs.length; i++) {
         if (srcs[i] == imgs.getAttribute("src")) {
             imgs.setAttribute("src", srcs[(i + 1) % srcs.length]);
             break;
@@ -118,6 +160,26 @@ function changeInfo() {
         document.getElementById("description").style.display = "none";
         document.getElementById("review").style.display = "block";
     }
+}
+
+function editName() {
+    console.log("edit name");
+}
+function editDescription() {
+    console.log("edit description");
+
+}
+function editPrice() {
+    console.log("edit price");
+
+}
+function editImage() {
+    console.log("edit image");
+
+}
+function editTag() {
+    console.log("edit tag");
+
 }
 
 window.addEventListener("load", start, false);
