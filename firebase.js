69b58f127502f3ca58ac441eef4c085cbc7e6f46
shenks0628 <cooljoseph100@gmail.com>
2023-12-14 13:34:38 +0800

    add: index display

--- firebase.js
+++ firebase.js
@@ -27,12 +27,14 @@ const db = getFirestore(app);
 const display = async () => {
     const latest_normal = document.getElementById("latest_normal");
     const latest_bids = document.getElementById("latest_bids");
+    const prev_view = document.getElementById("prev_view");
     const q_noraml = query(collection(db, "products"), where("type", "==", "normal"), orderBy("time", "desc"), limit(20));
     const q_bids = query(collection(db, "products"), where("type", "==", "bids"), orderBy("time", "desc"), limit(20));
     const queryNormalSnapshot = await getDocs(q_noraml);
     const queryBidsSnapshot = await getDocs(q_bids);
     latest_normal.innerHTML = "";
     latest_bids.innerHTML = "";
+    prev_view.innerHTML = "";
     queryNormalSnapshot.forEach((doc) => {
         console.log(doc.id, "=>", doc.data());
         const productData = doc.data();
@@ -52,6 +54,37 @@ const display = async () => {
             }
         }
         latest_bids.innerHTML += '<div class="product" id="' + doc.id + '"><a href="api/index.html?id=' + doc.id + '"><img src="' + productData.imgs[0] + '" alt="product"></a><h3>' + productName +  '</h3><p>結標時間：<a class="price">' + endDate.toLocaleString() + '</a></p><p>目前競價：</p><p class="price">' + productData.price + '</p><button class="btn">加入競標清單</button></div>';
+    });
+    onAuthStateChanged(auth, async(user) => {
+        if (user) {
+            const userId = user.email;
+            const docSnap = await getDoc(doc(db, "users", userId));
+            if (docSnap.exists()) {
+                const views = docSnap.data().view;
+                views.forEach(async (productId) => {
+                    const productData = await getDoc(doc(db, "products", productId));
+                    const productName = productData.name.split('#')[0];
+                    const productType = productData.type;
+                    if (productType == "normal") {
+                        prev_view.innerHTML += '<div class="product" id="' + productId + '"><a href="api/index.html?id=' + productId + '"><img src="' + productData.imgs[0] + '" alt="product"></a><h3>' + productName +  '</h3><p>不二價：</p><p class="price">' + productData.price + '</p><button class="btn">加入購物車</button></div>';;
+                    }
+                    else if (productType == "bids") {
+                        let endDate = productData.endtime.toDate();
+                        if (productData.bids_info.modtime) {
+                            const tmpDate = productData.bids_info.modtime.toDate();
+                            tmpDate.setHours(tmpDate.getHours() + 8);
+                            if (tmpDate < endDate) {
+                                endDate = tmpDate;
+                            }
+                        }
+                        prev_view.innerHTML += '<div class="product" id="' + productId + '"><a href="api/index.html?id=' + productId + '"><img src="' + productData.imgs[0] + '" alt="product"></a><h3>' + productName +  '</h3><p>結標時間：<a class="price">' + endDate.toLocaleString() + '</a></p><p>目前競價：</p><p class="price">' + productData.price + '</p><button class="btn">加入競標清單</button></div>';
+                    }
+                });
+            }
+        }
+        else {
+
+        }
     });
 }
 
