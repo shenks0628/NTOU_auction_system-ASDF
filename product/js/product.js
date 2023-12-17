cddb99c353fa47be1c2c4450ed0424e88b32a328
DHugo324 <hugodai0324@gmail.com>
2023-12-17 15:10:35 +0800

    delete something not important

--- product/js/product.js
+++ product/js/product.js
@@ -1,10 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, updateProfile, signOut, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore, collection, doc, setDoc, getDoc, addDoc, getDocs, query, orderBy, limit, where, onSnapshot, deleteDoc, updateDoc, arrayUnion, arrayRemove, serverTimestamp, deleteField } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
+// import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-storage.js";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -18,15 +18,14 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 const auth = getAuth();
 const db = getFirestore(app);
-const storage = getStorage();
+// const storage = getStorage();
 
 var imgs;
 var userID = "none", productOwnerID = "none";
 let productData;
-let oldProductData, newProductData;
 let id = "dd6VioVhhtD3p6P2r49r";
 const urlParams = new URLSearchParams(window.location.search);
 if (urlParams.get('id')) {
@@ -42,8 +41,6 @@ const getProduct = async () => { // 讀資料
             if (productDoc.exists()) {
                 // 取得該產品的資料
                 productData = productDoc.data();
-                oldProductData = productData;
-                newProductData = productData;
                 productOwnerID = productData.seller;
                 setting(userID, productOwnerID);
                 console.log("Product data for product with ID", productId, ":", productData);
@@ -62,12 +59,9 @@ function start() {
     eventSetting();
     getProduct();
     onAuthStateChanged(auth, (user) => {
-        // let login = document.getElementById("login");
-        // let profileImage = document.getElementById("profileImage");
         console.log(user);
         if (user) { // 登入狀態
             userID = user.email;
-            // profileImage.setAttribute("src", user.photoURL);
         }
         else {
             userID = "none";
@@ -202,144 +196,6 @@ function changeInfo() { // 切換商品資訊/評論
 function toEditPage() {
     window.location.href = "../edit?id=" + id;
 }
-/*
-function edit() { // 修改商品頁面
-    // window.alert("目前開放部分修改");
-    // console.log(newProductData);
-    // let str = newProductData.name.trim().split("#");
-    // document.getElementById("inputName").value = str[0];
-    // document.getElementById("inputDescription").value = newProductData.description;
-    // document.getElementById("inputPrice").value = newProductData.price;
-    // document.getElementById("inputQuantity").value = newProductData.quantity;
-    // if (str[1])
-    //     document.getElementById("inputTag1").value = str[1];
-    // if (str[2])
-    //     document.getElementById("inputTag2").value = str[2];
-    // if (str[3])
-    //     document.getElementById("inputTag3").value = str[3];
-    // let updateImage = newProductData.imgs;
-    // let oldImage = document.getElementById("oldImage");
-    // oldImage.innerHTML = "";
-    // for (let i = 0; i < updateImage.length; i++) {
-    //     var img = document.createElement("img");
-    //     img.setAttribute("src", updateImage[i]);
-    //     img.setAttribute("alt", updateImage[i]);
-    //     img.setAttribute("height", "50px");
-    //     img.setAttribute("width", "50px");
-    //     img.setAttribute("title", "點擊以刪除圖片");
-    //     img.style.cursor = "pointer";
-    //     img.onclick = temporaryDeleteImage(img, updateImage[i]);
-    //     oldImage.appendChild(img);
-    // }
-    // document.getElementById("editPage").style.display = "block";
-}
-
-function temporaryDeleteImage(img, src) {
-    return function updateBeDelted() {
-        img.style.display = "none";
-        for (let i = 0; i < newProductData.imgs.length; i++) {
-            if (src == newProductData.imgs[i]) {
-                newProductData.imgs.splice(i, 1);
-                break;
-            }
-        }
-        // console.log(newProductData.imgs);
-    }
-}
-function closeEditPage() { // 關閉修改頁面
-    if (window.confirm("關閉將不會儲存本次修改，是否確認？")) {
-        document.getElementById("editPage").style.display = "none";
-        newProductData = oldProductData;
-    }
-}
-function temporaryStore() { // 暫存修改紀錄
-    document.getElementById("editPage").style.display = "none";
-    newProductData.name = document.getElementById("inputName").value + ("#" + document.getElementById("inputTag1").value) + ("#" + document.getElementById("inputTag2").value) + ("#" + document.getElementById("inputTag3").value);
-    newProductData.price = parseInt(document.getElementById("inputPrice").value);
-    newProductData.quantity = parseInt(document.getElementById("inputQuantity").value);
-    oldProductData = newProductData;
-}
-const updateProduct = async () => { // 修改並更新資料庫
-    try {
-        const productId = id; // 替換成實際的產品 ID
-        // 使用 doc 函數構建該產品的參考路徑
-        const productRef = doc(db, "products", productId);
-        let name = document.getElementById("inputName").value;
-        if (document.getElementById("inputTag1").value.length != 0) name += ("#" + document.getElementById("inputTag1").value);
-        if (document.getElementById("inputTag2").value.length != 0) name += ("#" + document.getElementById("inputTag2").value);
-        if (document.getElementById("inputTag3").value.length != 0) name += ("#" + document.getElementById("inputTag3").value);
-        await updateDoc(productRef, {
-            // bids_info: {},
-            // comment: {},
-            // type: type,
-            imgs: newProductData.imgs,
-            name: name,
-            description: document.getElementById("inputDescription").value,
-            price: parseInt(document.getElementById("inputPrice").value),
-            quantity: parseInt(document.getElementById("inputQuantity").value),
-            // time: serverTimestamp(),
-            // url: ""
-        });
-        updateImage();
-        window.alert("修改成功！");
-        getProduct();
-        document.getElementById("editPage").style.display = "none";
-    } catch (err) {
-        console.error("Error: ", err);
-    }
-}
-const updateImage = async () => {
-    let inputImage = document.getElementById("inputImage").files;
-    for (let i = 0; i < inputImage.length; i++) {
-        const storageRef = ref(storage, "images/" + inputImage[i].name);
-        await uploadBytes(storageRef, inputImage[i]).then((snapshot) => {
-            console.log("Upload Success");
-        });
-        await getDownloadURL(storageRef).then(async (url) => {
-            await updateDoc(doc(db, "products", id), {
-                imgs: arrayUnion(url)
-            });
-        });
-    }
-    for (let i = 0; i < productData.imgs.length; i++) {
-        if (newProductData.imgs.includes(productData.imgs[i])) continue;
-        deleteStorageFile(productData.imgs[i]);
-    }
-}
-function preview() { // 預覽 目前不想做
-    // let str = newProductData.name.trim().split("#");
-    // let itemName = document.getElementById("itemName");
-    // itemName.innerHTML = str[0];
-    // let itemDescription = document.getElementById("itemDescription");
-    // itemDescription.innerHTML = newProductData.description;
-    // let itemPrice = document.getElementById("itemPrice");
-    // itemPrice.innerHTML = "$" + newProductData.price.toString();
-    // let itemTag = document.getElementById("itemTag");
-    // if (str.length == 1) itemTag.innerHTML = "無";
-    // else {
-    //     itemTag.innerHTML = "";
-    //     for (var i = 1; i < str.length; i++) {
-    //         itemTag.innerHTML += str[i];
-    //         if (i != str.length - 1) itemTag.innerHTML += ", "
-    //     }
-    // }
-
-    // let srcs = newProductData.imgs;
-    // imgs.setAttribute("src", srcs[0]);
-}
-function deleteStorageFile(fileUrl) {
-    const fileRef = ref(storage, fileUrl);
-
-    // Delete the file
-    deleteObject(fileRef).then(() => {
-        // File deleted successfully
-        console.log("delete complete");
-    }).catch((error) => {
-        console.log(error);
-        // Uh-oh, an error occurred!
-    });
-}
-*/
 window.addEventListener("load", start, false);
 
 // 初始化時顯示購物車內容
